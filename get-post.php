6080e38994364566a9631dd6c3b10958488cb35d
Bishal Tamang <tbishal0088@gmail.com>
2024-08-17 21:40:33 +0545

    Initial commit

---
+++ get-post.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="get-post.php" method="POST">
+    <label for="Name">Name</label>
+        <input type="text" name="name">
+    <label for="Name">password</label>
+        <input type="password" name="password">
+        <input type="submit" value="login">
+    </form>
+</body>
+</html>
+<?php
+echo "{$_POST["name"]}<br>";
+echo $_POST["password"];
+?>
