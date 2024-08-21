ed78f085416388845078ee7bf31ad31320547f54
Bishal Tamang <tbishal0088@gmail.com>
2024-08-22 00:06:33 +0545

    day-4

---
+++ Day-4/stringFunction.php
@@ -0,0 +1,15 @@
+<?php
+ $name="Bishal Tamang";
+ $phone="987-987-987";
+//  $lower=strtolower($name); changing string to lower case 
+//  $upper=strtoupper($name); changing string to upper case 
+//  $remove=trim($name); removing the unwanted space before and after the string 
+// $name=str_pad($name, 20,".");adding extra keyword up to the max size given
+// $phone=str_replace("9","1",$phone); replacing the string
+// $name=strrev($name); reversing the string
+// $name=str_shuffle($name);shuffling the string like card
+// $name=strcmp("$name", "Bishal Tamang"); comparing the string
+// $name=strlen($name); checking the length of the string
+// $lastname=substr($name,7,13);  extracting the specific string  
+ echo $name;
+?>
