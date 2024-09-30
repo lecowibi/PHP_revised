89c90ed0ab5ebaf3837f22d4e94675d1f4f345c5
Bishal Tamang <tbishal0088@gmail.com>
2024-10-01 00:13:05 +0545

    Update loginpage.php

--- Day-10/loginpage.php
+++ Day-10/loginpage.php
@@ -21,27 +21,3 @@ include("db.php");
     </form>
 </body>
 </html>
-<?php
-if($_SERVER["REQUEST_METHOD"] =="POST"){
-    $username=filter_input(INPUT_POST,"username",FILTER_SANITIZE_SPECIAL_CHARS);
-    $password=filter_input(INPUT_POST,"password",FILTER_SANITIZE_SPECIAL_CHARS);
-    if(empty($username)){
-        echo "Please Enter Username";
-    }
-    elseif(empty($password)){
-        echo "Please Enter Password";
-    }
-    else{
-        $hash= password_hash($password,PASSWORD_DEFAULT);
-        $sql ="INSERT INTO registrationformprj(username,password) VALUES ('$username','$hash')";
-        try{
-            mysqli_query($conn,$sql);
-            echo "You form are submitted succesfully";
-        }
-       catch(mysqli_sql_exception){
-        echo "The username is already taken";
-       }
-    }
-}
-    mysqli_close($conn);
-?>
